a6092913b4421254d3ca4c8804f4fb62a30e66cc
Akvilion <ianko.andrii@gmail.com>
2021-05-03 18:01:55 +0300

    disable if_match

--- settings.py
+++ settings.py
@@ -3,6 +3,10 @@ MONGO_URI = 'mongodb://localhost:27017/eve-course'
 
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']  # добавляємо дозволені методи
 
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']  # GET=read, PATCH=edit, PUT=replace
+
+IF_MATCH=False
+
 # модель для нашого people
 schema = {
     'firstname': {'type': 'string'},
