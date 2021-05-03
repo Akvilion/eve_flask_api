5880c2cbc5a0d7636f268e5cf1ba6fe4ee483549
Akvilion <ianko.andrii@gmail.com>
2021-05-03 17:47:54 +0300

    implement model for people endpoint

--- settings.py
+++ settings.py
@@ -1,6 +1,18 @@
 # mongo connection
 MONGO_URI = 'mongodb://localhost:27017/eve-course'
 
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']  # добавляємо дозволені методи
+
+# модель для нашого people
+schema = {
+    'firstname': {'type': 'string'},
+    'lastname': {'type': 'string'},
+}
+
+
+# 
 DOMAIN = {
-    'people': {},
+    'people': {
+        'schema': schema 
+    },
 }
