edd4dfe3e3a85e29ceab31aa3c337077a90b8b32
Akvilion <ianko.andrii@gmail.com>
2021-05-03 18:55:20 +0300

    write works enpoint with method limits

--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
 # mongo connection
 MONGO_URI = 'mongodb://localhost:27017/eve-course'
 
+# мотоди доступні для ресурсів
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']  # добавляємо дозволені методи
 
+# методи доступні для items
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']  # GET=read, PATCH=edit, PUT=replace
 
-IF_MATCH=False
+# відключення перевірки if_match 
+IF_MATCH = False
 
 # модель для нашого people
 schema = {
@@ -18,5 +21,9 @@ schema = {
 DOMAIN = {
     'people': {
         'schema': schema 
+    },
+    'works': {
+        'resource_methods': ['GET'],
+        'item_methods': ['GET'],
     },
 }
